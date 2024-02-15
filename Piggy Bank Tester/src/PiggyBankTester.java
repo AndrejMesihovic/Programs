6267b7efa6ec7ca60f5ff05bce224aac2e70d877
Andrej Mesihovic <159497008+AndrejMesihovic@users.noreply.github.com>
2024-02-14 21:40:22 -0700

    Update PiggyBankTester.java

--- Piggy Bank Tester/src/PiggyBankTester.java
+++ Piggy Bank Tester/src/PiggyBankTester.java
@@ -4,9 +4,8 @@ public class PiggyBankTester
 {
 public static void main(String[] args)
 {
-String input = ""; // this is a temp placeholder for user input
+String input = ""; 
 
-// Setup a dialog input and request the number of coins to put in the piggy bank
 input = JOptionPane.showInputDialog("Enter the number of pennies ");
 int pennies = Integer.parseInt(input);
 input = JOptionPane.showInputDialog("Enter the number of nickels ");
@@ -16,7 +15,6 @@ int dimes = Integer.parseInt(input);
 input = JOptionPane.showInputDialog("Enter the number of quarters ");
 int quarters = Integer.parseInt(input);
 
-// Create a piggy bank object by creating an instance of the piggy bank class with user specified input (coins)
 PiggyBank out = new PiggyBank(pennies, dimes, nickels, quarters);
 
 System.out.printf("Pennies = %d ",out.getPennies() );
@@ -25,10 +23,6 @@ System.out.printf("Dimes = %d ",out.getDimes());
 System.out.printf("Quarters = %d\n",out.getQuarters() );
 System.out.printf("Total coins in the piggy bank = %d\n", out.numCoins());
 System.out.printf("Piggy bank valuue = $%1.2f", out.totalValue());
-
-// Display the total number of each coin, the total number of coins and
-// the total value in the piggy bank.
-
 
 }
 }
